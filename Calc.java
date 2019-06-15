1d80df66f678187ceeee1eb2a6cec10d570f31c1
Liam Murphy <liam@spock.attlocal.net>
2019-06-15 17:08:25 -0500

    started Calc.java

--- Calc.java
+++ Calc.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Calc {
-	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		ExpParser parser = new ExpParser();
+		Stack stack = new Stack();
+
+		
+
+	}
+
+
 }
